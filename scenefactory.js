3460bdcf9e35d2fc1d37af8d02ec7c3ce69a50c9
Yosiya Hinosawa <stibium121@gmail.com>
2013-04-20 15:17:49 +0900

    changed prototype.constructor of factoried classes, because I think it is more suitable for further inheritance of created classes.

--- scenefactory.js
+++ scenefactory.js
@@ -26,7 +26,7 @@ window.FactoryFactory = function (parent, modifier) {
 
             var classPrototype = Class.prototype = exports.prototype = new parent();
 
-            classPrototype.constructor = exports;
+            classPrototype.constructor = Class;
 
             modifier(additionals);
 
@@ -37,47 +37,6 @@ window.FactoryFactory = function (parent, modifier) {
             return exports;
         }());
     };
-};
-
-window.SceneFactory = function (additionals) {
-    'use strict';
-
-    return (function () {
-        var exports = function () {
-            return new Scene();
-        };
-
-        var Scene = function () {
-            constructor.apply(this, arguments);
-        };
-
-        var constructor = additionals.constructor;
-        delete additionals.constructor;
-
-        var parent = window.scene;
-
-        var scenePrototype = Scene.prototype = exports.prototype = new parent();
-
-        scenePrototype.constructor = exports;
-
-        Function.prototype.E = function (dtor) { return dtor(this); };
-
-        scenePrototype.onEnter = additionals.onEnter.E(window.scene.OnEnterMethod);
-
-        scenePrototype.onExit = additionals.onExit.E(window.scene.OnExitMethod);
-
-        Object.keys(additionals).forEach(function (key) {
-            if (key === 'onExit' || key === 'onEnter') {
-                return;
-            }
-
-            scenePrototype[key] = additionals[key];
-        });
-
-        delete Function.prototype.E;
-
-        return exports;
-    }());
 };
 
 window.SceneFactory = window.FactoryFactory(window.scene, function (scenePrototype) {
